a190c4c13e9cf521326d583b837a79d19a0ad589
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-10-19 14:32:49 +0200

    t

--- autres/test.rb
+++ autres/test.rb
@@ -2,7 +2,7 @@ MYSQL_HOST='127.0.0.1'
 MYSQL_PORT='5000' -P${MYSQL_PORT} -h${MYSQL_HOST}
 mysql -uroot -proot -P 5000 -h 127.0.0.1
 pour chercher sur github -> git clone https://github.com/mgrosmann/fifa.git
-pour mettre à jour de github vers local -> cd repository git pull origin main
+pour mettre à jour de github vers local -> cd repository git pull origin main (git stash pour supprimer les modifs locales)
 pour mettre à jour de local vers github -> git add .  git commit -m "update"  git push origin main
 
 
