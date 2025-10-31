f571410f856aebe5299c78f317e87a6e538c97cd
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-10-31 18:14:41 +0100

    alias.sh

--- autres/alias.sh
+++ autres/alias.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "alias sql='mysql -u root -proot -h127.0.0.1 -P5000'" >> ~/.bashrc
-echo "alias sql14='mysql -u root -proot -h127.0.0.1 -DFIFA14 -P5000'" >> ~/.bashrc
-echo "alias sql15='mysql -u root -proot -h127.0.0.1 -DFIFA15 -P5000'" >> ~/.bashrc
-echo "alias sql16='mysql -u root -proot -h127.0.0.1 -DFIFA16 -P5000'" >> ~/.bashrc
-echo "alias sql18='mysql -u root -proot -h127.0.0.1 -DFIFA1518 -P5000'" >> ~/.bashrc
-echo "penser à faire source ~/.bashrc"
+echo "alias sql='mysql -u root -proot -h127.0.0.1 -P5000 -A'" >> ~/.bashrc
+echo "alias sql14='mysql -u root -proot -h127.0.0.1 -DFIFA14 -P5000 -A'" >> ~/.bashrc
+echo "alias sql15='mysql -u root -proot -h127.0.0.1 -DFIFA15 -P5000 -A'" >> ~/.bashrc
+echo "alias sql16='mysql -u root -proot -h127.0.0.1 -DFIFA16 -P5000 -A'" >> ~/.bashrc
+echo "alias sql18='mysql -u root -proot -h127.0.0.1 -DFIFA1518 -P5000 -A'" >> ~/.bashrc
+echo "penser à faire 'source ~/.bashrc'"
