49cc5b7a2fe141ff4eb666be8f75ed5bbf1cf073
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-11-01 08:54:25 +0100

    Remove unused alias from alias.sh
    Remove unused alias 'vide' from alias.sh

--- autres/alias.sh
+++ autres/alias.sh
@@ -4,5 +4,4 @@ echo "alias sql14='mysql -u root -proot -h127.0.0.1 -DFIFA14 -P5000 -A'" >> ~/.b
 echo "alias sql15='mysql -u root -proot -h127.0.0.1 -DFIFA15 -P5000 -A'" >> ~/.bashrc
 echo "alias sql16='mysql -u root -proot -h127.0.0.1 -DFIFA16 -P5000 -A'" >> ~/.bashrc
 echo "alias sql18='mysql -u root -proot -h127.0.0.1 -DFIFA1518 -P5000 -A'" >> ~/.bashrc
-echo "alias vide='> '" >> ~/.bashrc
 echo "penser à faire 'source ~/.bashrc'"
