e915d10a9711a6689f2bd07344daeb296e920b28
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-11-08 15:30:56 +0100

    sss

--- autres/test.sh
+++ autres/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cmd="mysql -uroot -proot -h127.0.0.1 -DFIFA${DB} -P5000 -A"
 read -p "Nom de la base de données (juste les 2 chiffres ex: 14, 15) : " DB
+cmd="mysql -uroot -proot -h127.0.0.1 -DFIFA${DB} -P5000 -A"
 txt_dir="/mnt/c/github/txt/FIFA${DB}/"
 target_dir="/mnt/c/github/txt/FIFA${DB}/csv/"
 #partie convertir utf16 en utf8 et txt en csv
@@ -9,7 +9,8 @@ for file in "$txt_dir"/*.txt; do
     if [ -f "$file" ]; then
         base_name=$(basename "$file" .txt)
         tmp_csv="/tmp/${base_name}.csv"
-        iconv -f UTF-16 -t UTF-8 "$file" -o "$tmp_csv" 2>/dev/null || cp $file $tmp_csv
+        iconv -f UTF-16 -t UTF-8 "$file" -o "$tmp_csv" 2>/dev/null || cp "$file" "$tmp_csv"
+        dos2unix "$tmp_csv"
         tr '\t' ';' < "$tmp_csv" > "$target_dir/${base_name}.csv"
         rm "$tmp_csv"
     fi
@@ -23,7 +24,7 @@ for file in "$target_dir"/*.csv; do
         echo "CREATE TABLE \`$table_name\` (test VARCHAR(255));" >> "$create_table_sql"
     fi
 done
-$cmd" < "$create_table_sql"
+$cmd < "$create_table_sql"
 # requete sql pour créer les colonnes dans les tables à partir de fichiers csv dans un répertoire
 create_columns_sql="create_columns.sql"
 for file in "$target_dir"/*.csv; do
@@ -34,7 +35,7 @@ for file in "$target_dir"/*.csv; do
         done
     fi
 done
-$cmd" < "$create_columns_sql"
+$cmd < "$create_columns_sql"
 #supprimer la colonne test des tables créées
 delete_columns_sql="delete_columns.sql"
 for file in "$target_dir"/*.csv; do
@@ -43,4 +44,4 @@ for file in "$target_dir"/*.csv; do
         echo "ALTER TABLE \`$table_name\` DROP COLUMN IF EXISTS \`test\`;" >> "$delete_columns_sql"
       fi
 done
-$cmd" < "$delete_columns_sql"
+$cmd < "$delete_columns_sql"
