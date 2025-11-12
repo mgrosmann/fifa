8c5d978364a86fa25d378bc1953b4d94b783dafe
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-11-12 11:52:18 +0100

    h

--- sql/name-id.sh
+++ sql/name-id.sh
@@ -2,8 +2,9 @@
 DB_NAME="fifa14"
 cmd="mysql -uroot -proot -h127.0.0.1 -D ${DB_NAME} -A -e"
 $cmd "
--- 1️⃣ Ajouter une ligne vide pour les surnoms si elle n'existe pas
 INSERT IGNORE INTO playernames (name, commentaryid) VALUES ('', 900000);
 
--- 2️⃣ Mettre à jour les joueurs sans surnom pour pointer sur cette ligne
-UPDATE players SET commonnameid=LAST_INSERT_ID() WHERE commonnameid=0;
+SET @empty_id = (SELECT nameid FROM playernames WHERE name='' LIMIT 1);
+
+-- Mettre à jour tous les joueurs sans surnom
+UPDATE players SET commonnameid=@empty_id WHERE commonnameid=0;"
