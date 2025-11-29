0fbbfb3761db801bb619e38c2bc0dd15f91f1828
mgrosmann <150331160+mgrosmann@users.noreply.github.com>
2025-11-29 19:25:12 +0100

    ss

--- cmtracker/import-cmtracker.sh
+++ cmtracker/import-cmtracker.sh
@@ -188,9 +188,9 @@ do
 
     KEY=$($MYSQL_CMD --skip-column-names -e \
         "SELECT IFNULL(MAX(artificialkey)+1,1) FROM teamplayerlinks WHERE teamid=$tpl_teamid;")
-UPDATE teamplayerlinks
+$MYSQL_CMD -e "UPDATE teamplayerlinks
 SET artificialkey = artificialkey + 1
-WHERE artificialkey >= $key;
+WHERE artificialkey >= $key;"
     number=$($MYSQL_CMD --skip-column-names -e "
 SELECT COALESCE(MIN(tpl1.jerseynumber + 1),1)
 FROM teamplayerlinks tpl1
